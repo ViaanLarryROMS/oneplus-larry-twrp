e6ad497581a1f90876bb9ec6d58eeccd8e190739
ViaanROM <viaan152013@gmail.com>
2026-02-05 18:05:46 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2025 SebaUbuntu's pb device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,9 +9,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_OP5958L1.mk
+    $(LOCAL_DIR)/pb_OP5958L1.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_OP5958L1-user \
-    twrp_OP5958L1-userdebug \
-    twrp_OP5958L1-eng
+    pb_OP5958L1-user \
+    pb_OP5958L1-userdebug \
+    pb_OP5958L1-eng
